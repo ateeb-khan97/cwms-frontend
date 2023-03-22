bc053f3d50eab3c3001dd61bf5d8c5f92c2d0169
Ateeb Khan <ateebkhan997@gmail.com>
2023-03-22 13:09:35 +0500

    bug_resolved

--- modules/Transfer/transfer.type.ts
+++ modules/Transfer/transfer.type.ts
@@ -10,8 +10,8 @@ export default interface TransferType {
   location_from: string;
   location_to: string;
   stock_receive_date?: string;
-  loc_to_name: string;
-  loc_from_name: string;
+  loc_to_name?: string;
+  loc_from_name?: string;
   loc_name?: string;
   transfer_detail: TransferDetailType[];
   transfer_status: string;
