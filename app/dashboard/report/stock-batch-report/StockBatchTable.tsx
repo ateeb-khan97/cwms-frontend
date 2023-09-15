0543f6f2464c071d9a2f4319d90de62e5562b8a7
Muhammad Ateeb Khan <ateebkhan997@gmail.com>
2023-09-15 17:08:47 +0500

    stockbatchreport added

---
+++ app/dashboard/report/stock-batch-report/StockBatchTable.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import { Skeleton } from '@mantine/core';
+import DataTableComponent from 'components/Shared/DataTableComponent';
+import { useEffect, useState } from 'react';
+
+//
+interface IPropType {
+  getTableData: () => Promise<any[]>;
+}
+//
+export default function StockBatchTable({ getTableData }: IPropType) {
+  const [tableData, setTableData] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  //
+  async function tableDataSetter() {
+    setIsLoading(true);
+    setTableData(await getTableData());
+    setIsLoading(false);
+  }
+  useEffect(() => {
+    tableDataSetter();
+  }, []);
+  return (
+    <>
+      {isLoading ? (
+        <div className="w-full p-5">
+          <Skeleton height={8} radius="xl" />
+          <Skeleton height={8} mt={6} radius="xl" />
+          <Skeleton height={8} mt={6} width="70%" radius="xl" />
+        </div>
+      ) : (
+        <DataTableComponent
+          columns={[
+            {
+              name: 'Prod.ID',
+              selector: (row: any) => row.product_id,
+              center: true,
+              grow: 0,
+              width: '80px',
+            },
+            {
+              name: 'Product',
+              selector: (row: any) => row.product_name,
+            },
+            {
+              name: 'Manufacturer',
+              selector: (row: any) => row.manufacturer_name,
+            },
+            {
+              name: 'Category',
+              selector: (row: any) => row.category_name,
+            },
+            {
+              name: 'Batch No',
+              cell: (row: any) => row.batch_number,
+              grow: 0,
+              center: true,
+            },
+            {
+              name: 'Loc.Name',
+              selector: (row: any) => row.loc_name,
+              center: true,
+              grow: 0,
+              width: '150px',
+            },
+            {
+              name: 'Inventory',
+              selector: (row: any) => row.total_inventory,
+              center: true,
+              grow: 0,
+              width: '100px',
+            },
+
+            {
+              name: 'T.P',
+              selector: (row: any) => (row.trade_price ? row.trade_price : '-'),
+              center: true,
+              grow: 0,
+              width: '80px',
+            },
+            {
+              name: 'MRP',
+              selector: (row: any) =>
+                row.mrp_unit_price ? row.mrp_unit_price : '-',
+              center: true,
+              grow: 0,
+              width: '80px',
+            },
+          ]}
+          data={tableData}
+        />
+      )}
+    </>
+  );
+}
---
+++ app/dashboard/report/stock-batch-report/page.tsx
@@ -0,0 +1,68 @@
+import BreadcrumbComponent from 'components/Shared/BreadcrumbComponent';
+import StockBatchTable from './StockBatchTable';
+import prisma from 'config/prisma';
+import { cookies } from 'next/headers';
+
+function Header() {
+  return (
+    <header className="select-none text-[#3b3e66]">
+      <BreadcrumbComponent />
+      <h1 className="text-3xl font-semibold ">Stock Batch Report</h1>
+      <p>Please see Stock Batch Report below all connected channels</p>
+    </header>
+  );
+}
+//
+async function getTableData() {
+  'use server';
+  await prisma.$queryRawUnsafe('SET SQL_MODE = "";');
+  const loc = cookies().get('loc_no')?.value;
+  const type = cookies().get('type')?.value;
+  //
+  const userQuery = ` WHERE ind.location_id = ${loc} AND ind.bin_id IS NOT NULL AND ind.is_received = 1 GROUP BY ind.product_id, isku.batch_number, ind.location_id;`;
+  const adminQuery = ` WHERE ind.bin_id IS NOT NULL AND ind.is_received = 1 GROUP BY ind.product_id, isku.batch_number, ind.location_id;`;
+
+  return (await prisma.$queryRawUnsafe(`SELECT ind.product_id, isku.batch_number, (SELECT manufacturer_name FROM manufacturers LEFT JOIN products ON
+    products.manufacturer_id = manufacturers.id WHERE products.id = ind.product_id) AS manufacturer_name,
+  (SELECT group_concat(category_name)
+  FROM
+    product_category
+  LEFT JOIN categories ON
+    product_category.category_id = categories.id
+  WHERE
+    product_category.product_id = ind.product_id) AS category_name,
+  p.product_name,
+  ind.third_level as item_conversion,
+  (SUM(ind.third_level)) AS total_inventory,
+  loc.loc_name,
+  p.trade_price,
+  p.mrp_unit_price
+from
+  inward_detail ind
+LEFT JOIN inward_sku isku ON
+  isku.product_id = ind.product_id
+  AND isku.inward_id = ind.inward_id
+LEFT JOIN products p ON
+  p.id = ind.product_id
+LEFT JOIN locations loc ON
+  loc.loc_code = ind.location_id
+${type == 'admin' ? adminQuery : userQuery}`)) as any[];
+}
+//
+export default function Page() {
+  return (
+    <>
+      <section className="flex min-h-[100%] flex-col gap-10 p-7">
+        <Header />
+        <div className="rounded-md border border-gray-100 bg-white shadow-xl">
+          <div className="flex items-center justify-between border-b-[1px] p-5">
+            <p className="py-2 font-semibold text-gray-500">
+              Here you can manage your all Stock Batch Report!
+            </p>
+          </div>
+          <StockBatchTable getTableData={getTableData} />
+        </div>
+      </section>
+    </>
+  );
+}
