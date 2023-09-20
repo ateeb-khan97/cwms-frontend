45e881c7c2633a366e0772f5b312a359a3ab128d
Muhammad Ateeb Khan <ateebkhan997@gmail.com>
2023-09-20 11:56:31 +0500

    work done

--- app/dashboard/purchase_order/quality_check/page.tsx
+++ app/dashboard/purchase_order/quality_check/page.tsx
@@ -5,7 +5,7 @@ async function getDownloadData() {
   'use server';
   //
   return (await prisma.$queryRawUnsafe(
-    `SELECT id, po_id, product_id, product_name, required_quantity, received_quantity, remaining_quantity, maximum_retail_price, trade_price, discount_percentage, batch_number, CAST(DATE(batch_expiry) as CHAR) as batch_expiry, foc, uom, purchasing_price, comment, gst_rate, advance_income_tax, invoiceNumber, advanceIncome FROM grn WHERE qc_approved = false AND grn_status != "D";`,
+    `SELECT id, po_id, product_id, product_name, required_quantity, received_quantity, remaining_quantity, maximum_retail_price, trade_price, discount_percentage, batch_number, CAST(DATE(batch_expiry) as CHAR) as batch_expiry, foc, uom, purchasing_price, comments, gst_rate, advance_income_tax, invoiceNumber, advanceIncome FROM grn WHERE qc_approved = false AND grn_status != "D";`,
   )) as any[];
 }
 //
