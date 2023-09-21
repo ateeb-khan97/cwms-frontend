b21a4b22b794da5555db231d4fef4a6c906f2724
Muhammad Ateeb Khan <ateebkhan997@gmail.com>
2023-09-21 16:08:53 +0500

    workdone

--- components/Report/SkuChildReport.tsx
+++ components/Report/SkuChildReport.tsx
@@ -63,10 +63,10 @@ export default function SkuChildReport({
   //
   async function downloadHandler() {
     try {
-      const downloadData = await getDownloadData();
+      const downloadData = new Set(await getDownloadData());
       console.log(downloadData);
 
-      const csvData = Papa.unparse(downloadData);
+      const csvData = Papa.unparse(Array.from(downloadData));
       // Create a Blob containing the CSV data
       const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8' });
       saveAs(blob, 'sku_child.csv');
