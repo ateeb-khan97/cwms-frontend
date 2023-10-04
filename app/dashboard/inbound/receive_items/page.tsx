550beedc6f65adc9ef293963bb6907dffe376aba
Muhammad Ateeb Khan <ateebkhan997@gmail.com>
2023-10-04 11:52:52 +0500

    newQuery

--- app/dashboard/inbound/receive_items/page.tsx
+++ app/dashboard/inbound/receive_items/page.tsx
@@ -70,62 +70,35 @@ async function getTableData() {
   const isReceived = filter == 'Received';
   //
   const type = cookies().get('type')?.value;
-  const acc_no = cookies().get('acc_no')?.value;
   const loc_no = cookies().get('loc_no')?.value;
   const isAdmin = type == 'admin';
   //
-  const OR: any[] = [
-    { product_name: { contains: search } },
-    { received_quantity: { contains: search } },
-    { received_quantity: { contains: search } },
-    { maximum_retail_price: { contains: search } },
-    { trade_price: { contains: search } },
-    { discount_percentage: { contains: search } },
-    { batch_number: { contains: search } },
-    { batch_expiry: { contains: search } },
-    { inward_id: { contains: search } },
-    { inward_date: { contains: search } },
-    { user_id: { contains: search } },
-    { user_name: { contains: search } },
+  const searchTermsArray = [
+    `product_name LIKE "%${search}%"`,
+    `received_quantity LIKE "%${search}%"`,
+    `maximum_retail_price LIKE "%${search}%"`,
+    `trade_price LIKE "%${search}%"`,
+    `discount_percentage LIKE "%${search}%"`,
+    `batch_number LIKE "%${search}%"`,
+    `batch_expiry LIKE "%${search}%"`,
+    `inward_id LIKE "%${search}%"`,
+    `inward_date LIKE "%${search}%"`,
+    `user_id LIKE "%${search}%"`,
+    `user_name LIKE "%${search}%"`,
   ];
+  let searchTerms = searchTermsArray.join(' OR ');
   //
-  if (filter != '') {
-    if (isReceived) {
-      OR.push({ inward_id: { not: null } });
-    } else {
-      OR.push({ inward_id: null });
-    }
-  }
+  if (isAdmin) searchTerms += ` AND location_id = ${loc_no}`;
   //
-  return await prisma.inward_sku.findMany({
-    orderBy: [{ id: 'desc' }],
-    take: +currentRowsPerPage,
-    skip: (Number(page) - 1) * Number(currentRowsPerPage),
-    select: {
-      id: true,
-      po_id: true,
-      product_id: true,
-      product_name: true,
-      received_quantity: true,
-      maximum_retail_price: true,
-      trade_price: true,
-      discount_percentage: true,
-      batch_number: true,
-      batch_expiry: true,
-      foc: true,
-      inward_id: true,
-      inward_date: true,
-      user_id: true,
-      user_name: true,
-    },
-    where: isAdmin
-      ? { OR }
-      : {
-          account_number: acc_no,
-          location_id: loc_no,
-          OR,
-        },
-  });
+  return (await prisma.$queryRawUnsafe(`SELECT *
+  FROM inward_sku
+  WHERE
+      ${searchTerms}
+  ORDER BY id DESC
+  LIMIT ${currentRowsPerPage} OFFSET ${
+    (Number(page) - 1) * Number(currentRowsPerPage)
+  };
+  `)) as any[];
 }
 async function getDownloadData() {
   'use server';
